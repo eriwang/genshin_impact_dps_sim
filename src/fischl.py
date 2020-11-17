1dbc718afe02058342cf3c0d59a692a12523dbf1
Eric Wang <johnericcw@gmail.com>
2020-11-17 18:37:06 -0500

    Cleanup, add artifacts to the equation, axes

--- src/fischl.py
+++ src/fischl.py
@@ -1,39 +1,39 @@
+from collections import namedtuple
 import random
 
 _AUTO_ATTACK_DMG_PERCENTS = [0.441, 0.468, 0.581, 0.577, 0.721]
 _AUTO_ATTACK_TIME = 2.6 / 5
 
 
-class Fischl:
-    def __init__(self, weap_base_atk, weap_percent_atk, weap_crit_rate):
-        # Fischl 80/80
-        self._char_base_atk = 216
-        self._char_percent_atk = 0.18
-        self._char_crit_rate = 0.05
-        self._char_crit_dmg = 0.5
+CharStats = namedtuple('CharStats', ['base_atk', 'percent_atk', 'crit_rate', 'crit_dmg'])
+WeaponStats = namedtuple('WeaponStats', ['base_atk', 'percent_atk', 'crit_rate'])
+ArtifactStats = namedtuple('ArtifactStats', ['flat_atk', 'percent_atk', 'crit_rate', 'crit_dmg'])
 
-        self._weap_base_atk = weap_base_atk
-        self._weap_percent_atk = weap_percent_atk
-        self._weap_crit_rate = weap_crit_rate
+
+class Fischl:
+    def __init__(self, weapon_stats, artifact_stats):
+        self._char_stats = CharStats(216, 0.18, 0.05, 0.5)  # Fischl 80/80
+        self._weap_stats = weapon_stats
+        self._arti_stats = artifact_stats
 
         self._next_auto_index = 0
         # self._skill_ready_ts = 0
         # self._burst_ready_ts = 0
 
     def run_iteration(self, controller, current_time):  # pylint: disable=unused-argument
-        auto_attack_damage = _calculate_damage(self._char_base_atk + self._weap_base_atk,
-                                               self._char_percent_atk + self._weap_percent_atk,
-                                               0,
-                                               self._char_crit_rate + self._weap_crit_rate,
-                                               self._char_crit_dmg,
+        auto_attack_damage = _calculate_damage(self._char_stats, self._weap_stats, self._arti_stats,
                                                _AUTO_ATTACK_DMG_PERCENTS[self._next_auto_index])
 
         controller.perform_active_action(self, auto_attack_damage, _AUTO_ATTACK_TIME)
         self._next_auto_index = (self._next_auto_index + 1) % len(_AUTO_ATTACK_DMG_PERCENTS)
 
 
-def _calculate_damage(char_weap_base_atk, atk_percent_bonus, artifact_flat_atk_bonus, crit_rate, crit_dmg,
-                      skill_dmg_percent):
-    final_attack_stat = char_weap_base_atk * (1 + atk_percent_bonus) + artifact_flat_atk_bonus
+def _calculate_damage(char_stats, weap_stats, artifact_stats, skill_dmg_percent):
+    atk_percent_bonus = char_stats.percent_atk + weap_stats.percent_atk + artifact_stats.percent_atk
+    final_attack_stat = (char_stats.base_atk + weap_stats.base_atk) * (1 + atk_percent_bonus) + artifact_stats.flat_atk
+
+    crit_rate = char_stats.crit_rate + weap_stats.crit_rate + artifact_stats.crit_rate
+    crit_dmg = char_stats.crit_dmg + artifact_stats.crit_dmg
     crit_multiplier = (crit_dmg + 1) if (random.random() < crit_rate) else 1
+
     return final_attack_stat * skill_dmg_percent * crit_multiplier
--- src/graph_dps.py
+++ src/graph_dps.py
@@ -1,18 +1,24 @@
 from matplotlib import pyplot
 
 from controller import Controller
-from fischl import Fischl
+from fischl import WeaponStats, ArtifactStats, Fischl
 
 
 def main():
     controller = Controller()
-    trials = 1000
+    trials = 10000
 
-    # Rust 80/80
-    rust_trials = [controller.simulate_total_damage(Fischl(449, 0.377, 0), 10) for _ in range(trials)]
+    # Feather +20 ATK is 311
+    # Artifact +20 ATK% is 46.6, Crit rate is 31.3, dmg is 62.2
 
-    # Slingshot 80/80
-    slingshot_trials = [controller.simulate_total_damage(Fischl(314, 0, 0.285), 10) for _ in range(trials)]
+    rust = WeaponStats(449, 0.377, 0)  # Rust 80/80
+    crit_rate_artifacts = ArtifactStats(311, 2 * 0.466, 0.4, 0.2)
+    rust_trials = [controller.simulate_total_damage(Fischl(rust, crit_rate_artifacts), 10) for _ in range(trials)]
+
+    slingshot = WeaponStats(314, 0, 0.285)  # Slingshot 80/80
+    crit_dmg_artifacts = ArtifactStats(311, 2 * 0.466, 0.2, 0.8)
+    slingshot_trials = [controller.simulate_total_damage(Fischl(slingshot, crit_dmg_artifacts), 10)
+                        for _ in range(trials)]
 
     if trials == 1:
         print(rust_trials[0])
@@ -21,7 +27,10 @@ def main():
 
     pyplot.hist(rust_trials, alpha=0.5, label='Rust')
     pyplot.hist(slingshot_trials, alpha=0.5, label='Slingshot')
+    pyplot.xlabel('Total Damage over 10 seconds')
+    pyplot.ylabel('Frequency')
     pyplot.legend(loc='upper right')
+    pyplot.title('Estimated Simulated Damage for Rust v. Slingshot for Fischl Auto-Attacks')
     pyplot.show()
 
 
